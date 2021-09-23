b695209feb6238aa7b8a90f57be63ab310ab36b2
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-09-22 21:01:07 -0300

    feat(user): entidade de usuario implementada

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+import { Prisma } from '.prisma/client';
+
+export class User implements Prisma.UserUncheckedCreateInput {
+  id?: number;
+
+  username: string;
+
+  email: string;
+  password: string;
+}
