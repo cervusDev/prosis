69fa9fb68cf304f57e90465479c68f71da01572c
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-09-25 09:32:46 -0300

    feat(app): run

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
